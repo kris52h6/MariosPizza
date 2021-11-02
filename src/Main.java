f4c242c88b5344f3934f7e628d73032f32ef361a
kris52h6 <kris52h6@stud.kea.dk>
2021-11-02 10:41:16 +0100

    pizza constructor

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         UserInterface ui = new UserInterface();
+
+        Pizza piz = new Pizza("Hawaii", 1, new String[]{"ananas", "skinke"});
+        System.out.println(piz);
+
     }
 }
--- src/Pizza.java
+++ src/Pizza.java
@@ -1,2 +1,22 @@
+import java.util.Arrays;
+
 public class Pizza {
+    private String name;
+    private int id;
+    private String[] toppings;
+
+    public Pizza(String name, int id, String[] toppings) {
+        this.name = name;
+        this.id = id;
+        this.toppings = toppings;
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", toppings=" + Arrays.toString(toppings) +
+                '}';
+    }
 }
