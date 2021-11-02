e9829557e3dcdd90b724b7c543763c2d1a0a1433
Mikkelv93 <mikkelsc2@gmail.com>
2021-11-02 10:58:22 +0100

    commit mikkel

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ public class Main {
 	// write your code here
         UserInterface ui = new UserInterface();
 
-        Pizza piz = new Pizza("Hawaii", 1, new String[]{"ananas", "skinke"});
-        System.out.println(piz);
+
 
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,29 @@
 public class Menu {
+    private ArrayList<Pizza> menu = new ArrayList<>();
+
+    public Menu() {
+
+
+
+
+    //Alle pizzaer - Strings
+
+    Pizza vesuvio = new Pizza("Hawaii", 1, new String[]{"tomatsauce", "ost", "skinke", "oregano"});
+    System.out.println(vesuvio);
+
+    Pizza amerikaner = new Pizza ("Amerikaner", 1, new String[]{"tomatsauce", "ost", "oksefars", "oregano"})Pizza cacciatore = new
+
+                Pizza cacciatore = new Pizza ("Cacciatore", 1, new String[]{"tomatsauce", "ost", "pepperoni", "oregano"});("Cacciatore", 1, new String[]{"tomatsauce", "ost", "pepperoni", "oregano"});
+
+    Pizza carbona = new Pizza ("Carbona", 1, new String[]{"tomatsauce", "ost", "kødsauce", "spaghetti", "cocktailpølser",  Pizza amerikaner = new Pizza ("Amerikaner", 1, new String[]{"tomatsauce", "ost", "oksefars", "oregano"});
+}
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Pizza pizza : menu) {
+            sb.append(pizza);
+        }
+        return sb.toString();
+    }
+
 }
